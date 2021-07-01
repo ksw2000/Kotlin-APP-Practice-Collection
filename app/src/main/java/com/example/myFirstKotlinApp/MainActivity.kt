abe6881eb2ace6323fb6832b1c9d2c6c0be70ad6
liao2000 <liao2000@livemail.tw>
2021-07-01 16:28:14 +0800

    Second commit

---
+++ app/src/main/java/com/example/myFirstKotlinApp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myFirstKotlinApp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.widget.TextView
+import com.example.myFirstKotlinApp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        var counter = 0
+        binding.fab.setOnClickListener { view ->
+            val txtCounter:TextView? = binding.root.findViewById<TextView>(R.id.counter)
+            counter++
+            txtCounter?.text = "$counter"
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
